5dcdf69d2a0f339f03ad1b912a15ad80bc740c82
Anchal Mulatkar <anchalmulatkar1@gmail.com>
2023-03-02 23:49:12 +0530

    Assignment2 two solution

---
+++ Assignment2/Arithmatic.py
@@ -0,0 +1,18 @@
+def Add(a,b):
+    c=a+b
+    return c
+
+def Sub(a,b):
+    c=a-b
+    return c
+
+def mul(a,b):
+    c=a*b
+    return c
+
+def div(a,b):
+    c=a/b
+    return c
+
+
+
---
+++ Assignment2/One.py
@@ -0,0 +1,18 @@
+from Arithmatic import *
+
+def main():
+    a=int(input("Enter a  First number="))
+    b=int(input("Enter a  Second number="))
+
+    print(Add(a,b))
+    print(Sub(a,b))
+    print(mul(a,b))
+    print(div(a,b))
+
+if __name__=="__main__":
+    main()
+
+
+
+
+
